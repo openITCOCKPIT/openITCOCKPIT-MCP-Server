b0ee1144f0dd4898cf5eecd2878b3b6b78ee9694
lukaskress <38035605+lukaskress@users.noreply.github.com>
2026-01-28 15:59:59 +0100

    Add functions to filter by host and servicestates

--- oitc_mcp.py
+++ oitc_mcp.py
@@ -9,16 +9,18 @@ import sys
 from pprint import pprint
 from datetime import datetime, timedelta
 from fastmcp import FastMCP
+
 mcp = FastMCP("MyServer")
 
 urllib3.disable_warnings()
 
 
-oitc_apikey = "2707a99d5e92f94d0a3c844b76573a0620102952ce5f476fde7b7a70039845ff9c02555d2a7ce674d91b217b090c5864466b08ddd5419477f901498f541d8ff7f9590a9d11f2c6299e2a6690380be96b"
-oitc_baseurl = "https://127.0.0.1"
-allservices={}
-allhosts={}
-wholeout=[]
+oitc_apikey = "d2083e61fee067077ac239b7d89084f31c16a66f5e2908f6f74009492b600d3d90db2c5f6175ff199c31e1adbdcf6cf089ecadaf1c93c776bc19c7f41feb42b58f2637883b1a1273035f0ade592415f5"
+oitc_baseurl = "https://demo.openitcockpit.io"
+allservices = {}
+allhosts = {}
+wholeout = []
+
 
 def oITC_APIRequest(method, url, payl) -> dict:
     headers = {"Authorization": "X-OITC-API " + oitc_apikey, "Content-Type": "application/json"}
@@ -33,65 +35,172 @@ def get_last_24hours_filter():
     return f"&filter[from]={yesterday.strftime(date_format)}&filter[to]={now.strftime(date_format)}"
 
 
-def GetAllServices():
-    resp, code = oITC_APIRequest("GET", "/services/index.json?angular=true&limit=9999", {})
-    if code != 200:
-        print(f"Error retrieving services: {resp}")
-        sys.exit(1)
-    services = resp.get("all_services", []) if isinstance(resp, dict) else resp
-    allservices.update({service["Service"]["uuid"]: service["Service"]["servicename"] for service in services})
-
-def GetAllHosts():
-    resp, code = oITC_APIRequest("GET", "/hosts/index.json?angular=true&limit=9999", {})
+def GetHostnameByUUID(uuid):
+    resp, code = oITC_APIRequest(
+        "GET",
+        f"/hosts/index.json?angular=true&filter%5BHosts.uuid%5D={uuid}",
+        {},
+    )
     if code != 200:
         print(f"Error retrieving hosts: {resp}")
         sys.exit(1)
-    hosts = resp.get("all_hosts", []) if isinstance(resp, dict) else resp
-    allhosts.update({host["Host"]["uuid"]: host["Host"]["hostname"] for host in hosts})
+    host = resp.get("all_hosts", []) if isinstance(resp, dict) else resp
+    if host:
+        return host[0]["Host"]["hostname"]
+
+
+def GetServiceNameByUUID(uuid):
+    resp, code = oITC_APIRequest(
+        "GET",
+        f"/services/index.json?angular=true&filter%5BServices.uuid%5D={uuid}",
+        {},
+    )
+    if code != 200:
+        print(f"Error retrieving services: {resp}")
+        sys.exit(1)
+    service = resp.get("all_services", []) if isinstance(resp, dict) else resp
+    if service:
+        return service[0]["Service"]["servicename"], service[0]["Host"]["hostname"]
+    
+def getServicesFromHost(id):
+    resp, code = oITC_APIRequest(
+        "GET",
+        f"/services/index.json?angular=true&scroll=true&sort=Services.id&filter[Hosts.id]={id}",
+        {},
+    )
+    if code != 200:
+        print(f"Error retrieving services: {resp}")
+        sys.exit(1)
+    filtered_services = []
+    for item in resp.get("all_services", []):
+
+        filtered_services.append(
+            {
+                "servicename": item["Service"].get("servicename"),
+                "description": item["Service"].get("description"),
+                "output": item["Servicestatus"].get("output"),
+                "long_output": item["Servicestatus"].get("long_output"),
+                "perfdata": item["Servicestatus"].get("perfdata"),
+                "lastCheck": item["Servicestatus"].get("lastCheck"),
+                "nextCheck": item["Servicestatus"].get("nextCheck"),
+                "outputHtml": item["Servicestatus"].get("outputHtml"),
+                "humanState": item["Servicestatus"].get("humanState"),
+            }
+        )
+    return filtered_services
+
 
 @mcp.tool
-def GetLogentries():
+def GetLast24hLogentries():
+    """Use this function if you want to get all log entries from the last 24 hours."""
     resp, code = oITC_APIRequest("GET", f"/logentries/index.json?angular=true&limit=250{get_last_24hours_filter()}", {})
     if code != 200:
         print(f"Error retrieving log entries: {resp}")
         sys.exit(1)
     entries = resp.get("logentries", []) if isinstance(resp, dict) else resp
-    
+
     for entry in entries:
         timestamp = entry.get("entry_time", "")
         if "SERVICE ALERT" in entry.get("logentry_data", ""):
-            host_name = allhosts.get(entry.get("logentry_data", "").split(";")[0].split(": ")[-1])
-            service_name = allservices.get(entry.get("logentry_data", "").split(";")[1])
+            service_name, host_name = GetServiceNameByUUID(entry.get("logentry_data", "").split(";")[1])
             service_state = entry.get("logentry_data", "").split(";")[2]
             service_output = entry.get("logentry_data", "").split(";")[5]
 
-            print(f"Time: {timestamp}, Host: {host_name}, Service: {service_name}, State: {service_state}, Output: {service_output}")
-            wholeout.append({
-                "time": timestamp,
-                "host": host_name,
-                "service": service_name,
-                "state": service_state,
-                "output": service_output
-            })
+            # print(f"Time: {timestamp}, Host: {host_name}, Service: {service_name}, State: {service_state}, Output: {service_output}")
+            wholeout.append(
+                {
+                    "time": timestamp,
+                    "host": host_name,
+                    "service": service_name,
+                    "state": service_state,
+                    "output": service_output,
+                }
+            )
         elif "HOST ALERT" in entry.get("logentry_data", ""):
-            host_name = allhosts.get(entry.get("logentry_data", "").split(";")[0].split(": ")[-1])
+            host_name = GetHostnameByUUID(entry.get("logentry_data", "").split(";")[0].split(": ")[-1])
             host_state = entry.get("logentry_data", "").split(";")[1]
             host_output = entry.get("logentry_data", "").split(";")[4]
 
-            print(f"Time: {timestamp}, Host: {host_name}, State: {host_state}, Output: {host_output}")
-            wholeout.append({
-                "time": timestamp,
-                "host": host_name,
-                "state": host_state,
-                "output": host_output
-            })
+            # print(f"Time: {timestamp}, Host: {host_name}, State: {host_state}, Output: {host_output}")
+            wholeout.append({"time": timestamp, "host": host_name, "state": host_state, "output": host_output})
 
     return wholeout
 
 
-    
-GetAllServices()
-GetAllHosts()
+@mcp.tool
+def GetHostinfo(hostname: str) -> list:
+    """Use this function if you want to get detailed information about a specific host."""
+    resp, code = oITC_APIRequest(
+        "GET",
+        f"/hosts/index.json?angular=true&filter%5BHosts.name%5D={hostname}",
+        {},
+    )
+    if code != 200:
+        print(f"Error retrieving host info: {resp}")
+        sys.exit(1)
+    # print(resp)
+
+    filtered_hosts = []
+
+    for item in resp.get("all_hosts", []):
+        host = item.get("Host", {})
+        status = item.get("Hoststatus", {})
+        filtered_services = getServicesFromHost(host.get("id"))
+        filtered_hosts.append(
+            {
+                "id": host.get("id"),
+                "uuid": host.get("uuid"),
+                "hostname": host.get("hostname"),
+                "address": host.get("address"),
+                "description": host.get("description"),
+                "lastCheck": status.get("lastCheck"),
+                "nextCheck": status.get("nextCheck"),
+                "output": status.get("output"),
+                "longoutput": status.get("long_output"),
+                "humanState": status.get("humanState"),
+                "outputHtml": status.get("outputHtml"),
+            }
+        )
+
+    return filtered_hosts, filtered_services
+
+@mcp.tool
+def getServicesbyState(state: str) -> list:
+    """Use this function if you want to get services by their state. Possible states are OK, WARNING, CRITICAL, UNKNOWN."""
+    resp, code = oITC_APIRequest(
+        "GET",
+        f"/services/index.json?angular=true&direction=desc&scroll=true&page=1&sort=Servicestatus.current_state&filter[Servicestatus.current_state]={state.lower()}",
+        {},
+    )
+    if code != 200:
+        print(f"Error retrieving services: {resp}")
+        sys.exit(1)
+    filtered_services = []
+    for item in resp.get("all_services", []):
+
+        filtered_services.append(
+            {
+                "hostname": item["Host"].get("hostname"),
+                "servicename": item["Service"].get("servicename"),
+                "description": item["Service"].get("description"),
+                "output": item["Servicestatus"].get("output"),
+                "long_output": item["Servicestatus"].get("long_output"),
+                "perfdata": item["Servicestatus"].get("perfdata"),
+                "lastCheck": item["Servicestatus"].get("lastCheck"),
+                "nextCheck": item["Servicestatus"].get("nextCheck"),
+                "outputHtml": item["Servicestatus"].get("outputHtml"),
+                "humanState": item["Servicestatus"].get("humanState"),
+            }
+        )
+        # print(filtered_services)
+    return filtered_services
+
+
+# GetAllServices()
+# GetAllHosts()
+# GetLast24hLogentries()
+# GetHostinfo("webserver01")
+# getServicesbyState("CRITICAL")
 
 if __name__ == "__main__":
     # Start an HTTP server on port 8000
